a889dce597eb0d5279af6734bab6309e10fbe9e3
kcx064 <kcx064@163.com>
2025-07-09 10:49:11 +0800

    change: bms_status code style adjust

--- src/modules/evtol_can/sensors/bms_status.hpp
+++ src/modules/evtol_can/sensors/bms_status.hpp
@@ -22,21 +22,21 @@ namespace fullymaxbms
 		struct bms_hcu_info_s
 		{
 			// 电池电压，高8位和低8位组合
-			uint8_t batVoltage_H : 8;
-			uint8_t batVoltage_L : 8;
+			uint8_t batVoltage_H;
+			uint8_t batVoltage_L;
 
 			// 电池电流，高8位和低8位组合
-			uint8_t batCurrent_H : 8;
-			uint8_t batCurrent_L : 8;
+			uint8_t batCurrent_H;
+			uint8_t batCurrent_L;
 
 			// 电池状态量
-			uint8_t batSoc : 8; //电池荷电状态
-			uint8_t batSoh : 8; //电池健康状态
+			uint8_t batSoc; //电池荷电状态
+			uint8_t batSoh; //电池健康状态
 			uint8_t batAlmLv : 4;//电池警告级别
 			uint8_t batState : 4;//电池状态
 
 			// 电池寿命
-			uint8_t batLife : 8;
+			uint8_t batLife;
 		}data;
 	}bms_hcu_info_t;
 
@@ -185,43 +185,7 @@ void bms_status::msg_cb(uint8_t canModule, uint32_t msg_id, uint8_t *rxData, uin
 
 	if(msg_id == msg_id_list[0])
 	{
-		memcpy(bms_hcu_info.data_raw, rxData, 8);
-		// _can_bms_status.timestamp = hrt_absolute_time();
-		// _can_bms_status.voltage_v = (static_cast<uint16_t>((bms_hcu_info.data.batVoltage_H << 8) | bms_hcu_info.data.batVoltage_L))*BMS_VOLTAGE_SCALE;
-		// _can_bms_status.voltage_filtered_v = _can_bms_status.voltage_v;
-
-		// _can_bms_status.current_a = -((static_cast<float>((bms_hcu_info.data.batCurrent_H << 8) | bms_hcu_info.data.batCurrent_L))*BMS_VOLTAGE_SCALE-1000.0f);//BMS_VOLTAGE_SCALE same as CURRENT_SCALE
-		// //厂家设置为放电电流为负值，充电电流为正值，且原始数据带有1000A偏置量。因此原始数据乘以电流转换系数，减去1000偏置量，取负值，即为实际电流值
-		// _can_bms_status.current_filtered_a = _can_bms_status.current_a;
-		// _can_bms_status.current_average_a = -1;
-
-		// _can_bms_status.cell_count = 12;
-		// _can_bms_status.scale = 1;
-		// _can_bms_status.voltage_cell_v[0] = _can_bms_status.voltage_v/12;//max: 53.4 equivalent 12cell * 4.45Vmax
-		// _can_bms_status.voltage_cell_v[1] = _can_bms_status.voltage_v/12;
-		// _can_bms_status.voltage_cell_v[2] = _can_bms_status.voltage_v/12;
-		// _can_bms_status.voltage_cell_v[3] = _can_bms_status.voltage_v/12;
-		// _can_bms_status.voltage_cell_v[4] = _can_bms_status.voltage_v/12;
-		// _can_bms_status.voltage_cell_v[5] = _can_bms_status.voltage_v/12;
-		// _can_bms_status.voltage_cell_v[6] = _can_bms_status.voltage_v/12;
-		// _can_bms_status.voltage_cell_v[7] = _can_bms_status.voltage_v/12;
-		// _can_bms_status.voltage_cell_v[8] = _can_bms_status.voltage_v/12;
-		// _can_bms_status.voltage_cell_v[9] = _can_bms_status.voltage_v/12;
-		// _can_bms_status.voltage_cell_v[10] = _can_bms_status.voltage_v/12;
-		// _can_bms_status.voltage_cell_v[11] = _can_bms_status.voltage_v/12;
-		// _can_bms_status.remaining = (_can_bms_status.voltage_v - 504.0f)/(640.8f - 504.0f);
-		// _can_bms_status.id = 4;
-		// _can_bms_status.temperature = NAN;
-		// _can_bms_status.time_remaining_s = NAN;
-		// _can_bms_status.connected = true;
-		// //144cells, MAX 4.45V/cell
-		// if(_can_bms_status.voltage_v < 640.8f)_can_bms_status.warning = battery_status_s::BATTERY_WARNING_NONE;//4.45
-		// if(_can_bms_status.voltage_v < 561.6f)_can_bms_status.warning = battery_status_s::BATTERY_WARNING_NONE;//3.9
-		// if(_can_bms_status.voltage_v < 547.2f)_can_bms_status.warning = battery_status_s::BATTERY_WARNING_NONE;//3.8
-		// if(_can_bms_status.voltage_v < 532.8f)_can_bms_status.warning = battery_status_s::BATTERY_WARNING_LOW;//3.7
-		// if(_can_bms_status.voltage_v < 504.0f)_can_bms_status.warning = battery_status_s::BATTERY_WARNING_EMERGENCY;//3.5
-
-		// _bms_status_pub.publish(_can_bms_status);
+		memcpy(&bms_hcu_info.data_raw, rxData, 8);
 
 
 		float_t bms_voltage = (static_cast<uint16_t>((bms_hcu_info.data.batVoltage_H << 8) | bms_hcu_info.data.batVoltage_L))*BMS_VOLTAGE_SCALE;
@@ -243,7 +207,7 @@ void bms_status::msg_cb(uint8_t canModule, uint32_t msg_id, uint8_t *rxData, uin
 	}
 	if(msg_id == msg_id_list[1])
 	{
-		memcpy(bms_hcu_alarm.data_raw, rxData, 8);
+		memcpy(&bms_hcu_alarm.data_raw, rxData, 8);
 
 		_fullymax_alarm.timestamp = hrt_absolute_time();
 		_fullymax_alarm.alm_cell_ov = bms_hcu_alarm.data.alm_cell_ov;
