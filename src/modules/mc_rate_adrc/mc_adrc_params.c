828ba1d2fa9c3b2aa70fdbe6fe3650defd78c99a
kcx064 <kcx064@163.com>
2026-06-01 18:07:18 +0800

    feat:初步增加adrc角速率模块

---
+++ src/modules/mc_rate_adrc/mc_adrc_params.c
@@ -0,0 +1,34 @@
+
+/**
+ * @file mc_adrc_params.c
+ *
+ * Parameters for Adrc behavior
+ */
+
+/**
+ * Observer bandwidth
+ *
+ * Observer bandwidth.
+ *
+ * @unit rad/s
+ * @min 0.0
+ * @max 2000.0
+ * @decimal 1
+ * @increment 5
+ * @group Multicopter ADRC
+ */
+PARAM_DEFINE_FLOAT(ADRC_BW_O, 10.f);
+
+/**
+ * Control bandwidth
+ *
+ * Control bandwidth.
+ *
+ * @unit rad/s
+ * @min 0.0
+ * @max 2000.0
+ * @decimal 1
+ * @increment 5
+ * @group Multicopter ADRC
+ */
+PARAM_DEFINE_FLOAT(ADRC_BW_C, 1.f);
