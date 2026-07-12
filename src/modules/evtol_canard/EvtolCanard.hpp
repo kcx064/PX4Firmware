d7ec21953b8be1717b7651ca2e1af00299c3e3ab
kcx064 <kcx064@163.com>
2026-07-12 21:42:42 +0800

    fix(evtol_canard): 修正编译问题可自动编译dsdl文件

--- src/modules/evtol_canard/EvtolCanard.hpp
+++ src/modules/evtol_canard/EvtolCanard.hpp
@@ -112,7 +112,6 @@ private:
 
 	// Parameters
 	DEFINE_PARAMETERS(
-		(ParamInt<px4::params::CANRVE_INTERVAL>) _param_db_interval,
 		(ParamInt<px4::params::CAN_BITRATE>) _param_db_can_rate,
 		(ParamInt<px4::params::CA_ROTOR_COUNT>) _ca_rotor_count,
 		(ParamInt<px4::params::EVTOL_NODE_ID>) _evtol_node_id
